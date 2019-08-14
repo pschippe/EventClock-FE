59e50c52bf26973e1e3143b384dfe4956205d5e9
patrick schippers <patrickschippers@swx-u-m-ETPatrick.frontierlocal.net>
2019-08-13 21:27:00 -0400

    Adds comment for creating multiple api methods

--- src/api.js
+++ src/api.js
@@ -5,6 +5,7 @@ class Api {
         baseURL: 'http://jsonplaceholder.typicode.com/'
     });
 
+    // Create another method for Master if changes are too extensive or if new api call is needed
     postTime (postObj) {
         return this.baseAxios.post(`/clockwerx/power`, {
             time: `${postObj.hours}:${postObj.minutes}:${postObj.seconds}`,
